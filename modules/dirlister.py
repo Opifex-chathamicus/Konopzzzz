d785372f5ccac45720adbcdf82db106201bc318b
alXCS <alexanderlemnos@hotmail.com>
2021-10-04 18:50:31 +0300

    Adding repo struct for konop

--- modules/dirlister.py
+++
@@ -1,5 +0,0 @@
-import os
-def run(**args):
-    print("[*]In dirlister modules.")
-    files=os.listdir(".") #lists all files in current directory
-    return str(files)
--- modules/environment.py
+++
@@ -1,4 +0,0 @@
-import os 
-def run(**args):
-    print("[*] In environment module.")
-    return str(os.environ) #retrieves any environment variables that are set on the remote machine.
