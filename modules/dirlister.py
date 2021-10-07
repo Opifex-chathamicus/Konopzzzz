901cb55f1d00d95ade4e23fc42dbc67833d4e348
Alex <52384614+alXCS@users.noreply.github.com>
2021-10-07 14:35:31 +0300

    Update dirlister.py
    Edited dirlister.py

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -4,3 +4,6 @@ def run(**args):
     print("[*]In dirlister modules.")
     files=os.listdir(".") #lists all files in current directory
     return str(files)
+
+if __name__ == '__main__':
+    run()
