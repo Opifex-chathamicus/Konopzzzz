7ac470e1f806043ef57427d0d7043db2d32a1381
alXCS <alexanderlemnos@hotmail.com>
2022-01-19 05:32:54 +0200

    Added konops.py and kontromain.py back again , the prototypes.

---
+++ kontromain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 15:27:19 2021
+
+@author: xcs
+"""
+
+import konops
+
+def main():
+    konopas=konops.Konops("obsv")
+    konopas.configure()
+    konopas.execute()
+    
+
+main()
